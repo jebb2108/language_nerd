f03d7bf2c89da1d7ead924d042671cb01d89085c
jebb <gabouchard2002@gmail.com>
2025-08-06 16:44:13 +0300

    tried another solution

--- bots/main_bot.py
+++ bots/main_bot.py
@@ -6,18 +6,13 @@
 
 Оба бота запускаются параллельно друг другу из разных файлов
 """
-import os
-import sys
 import asyncio
-import logging
-from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from web_launcher import start_web_app
-from routers import router as main_router
 from config import (
     init_global_resources,
     close_global_resources,
@@ -38,8 +33,9 @@ async def run():
     db_pool, session = await init_global_resources()
     # Запуск веб-сервера
     web_runner = await start_web_app(db_pool)
-    # Получение токена бота
+    # Получение токена бота и роутеров
     from config import BOT_TOKEN_MAIN
+    from routers import router as main_router
     # Инициализация бота
     bot = Bot(token=BOT_TOKEN_MAIN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     dp = Dispatcher(storage=storage)
