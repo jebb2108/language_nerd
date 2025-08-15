37a05a1e55d33e1ba49f551b9bc7be531e431518
Gabriel Bouchard <158368815+jebb2108@users.noreply.github.com>
2025-08-15 17:41:10 +0300

    Update registration callback handler
    Refactored

--- bots/routers/callback_handlers/registration_cb_handler.py
+++ bots/routers/callback_handlers/registration_cb_handler.py
@@ -6,7 +6,7 @@ from aiogram.fsm.state import StatesGroup, State
 from aiogram.types import CallbackQuery
 
 from middlewares.resources_middleware import ResourcesMiddleware # noqa
-from keyboards.inline_keyboards import show_language_keyboard, confirm_choice_keyboard # noqa
+from keyboards.inline_keyboards import show_language_keyboard, show_fluency_keyboard, confirm_choice_keyboard # noqa
 
 from config import LOG_CONFIG # noqa
 from translations import QUESTIONARY # noqa
@@ -26,42 +26,50 @@ async def handle_camefrom(callback: CallbackQuery, state: FSMContext):
     """
     await callback.answer()
 
-    try:
-        camefrom = callback.data.split("_", 1)[1]
-        await state.update_data(camefrom=camefrom)
-
-        data = await state.get_data()
-        lang_code = data.get("lang_code", "en")
-        message_mgr = data["message_mgr"]
-
-        await message_mgr.send_message_with_save(
-            chat_id=callback.message.chat.id,
-            text=QUESTIONARY["lang_pick"][lang_code],
-            reply_markup=show_language_keyboard(),
-        )
-
-    except Exception as e:
-        logger.error(f"Error in handle_camefrom: {e}")
+    camefrom = callback.data.split("_", 1)[1]
 
 
+    data = await state.get_data()
+    lang_code = data.get("lang_code", "en")
+    message_mgr = data["message_mgr"]
+
+    await message_mgr.send_message_with_save(
+        chat_id=callback.message.chat.id,
+        text=QUESTIONARY["lang_pick"][lang_code],
+        reply_markup=show_language_keyboard(),
+    )
+    
+    await state.update_data(camefrom=camefrom)
+    
+        
 @router.callback_query(F.data.startswith("lang_"))
-async def handle_language_choice(
-        callback: CallbackQuery,
-        state: FSMContext,
-        database: ResourcesMiddleware,
-):
+async def handle_fluency_choice(callback: CallbackQuery, state: FSMContext):
+    await callback.answer()
+    
+    data = await state.get_data()
+    lang_code = data.get("lang_code", "en")
+    
+    users_choice = callback.data.split("_", 1)[1]
+    msg = (
+            f"➪ Вы выбрали: {users_choice}\n\n"
+            f"QUESTIONARY["language"][lang_code]
+    )
+    await callback.message.edit_text(
+        text=msg,
+        reply_markup=show_fluency_keyboard(lang_code),
+    )
+    
+    await state.update_data(language=users_choice)
 
+
+@router.callback_query(F.data.startswith("fluency_"))
+async def handle_language_choice(callback: CallbackQuery, state: FSMContext):
     """
-    Сохраняем выбор языка, создаём запись в БД и идём в главное меню.
+    Сохраняем выбор языка
     """
     await callback.answer()
 
     data = await state.get_data()
-    user_id = data["user_id"]
-    username = data.get("username", "")
-    first_name = data.get("first_name", "")
-    camefrom = data.get("camefrom", "")
-    fluency = data.get("fluency", "in_making")
     lang_code = data.get("lang_code", "en")
 
     users_choice = callback.data.split("_", 1)[1]
@@ -75,18 +83,27 @@ async def handle_language_choice(
         text=msg,
         reply_markup=confirm_choice_keyboard(lang_code),
     )
-    # Сохраняем нового пользователя в БД
-    await database.create_user(user_id, username, first_name, camefrom, users_choice, fluency, lang_code)
-
+    
+    await state.update_data(fluency=users_choice)
 
 
 @router.callback_query(F.data == "action_confirm")
 async def go_to_main_menu(callback: CallbackQuery, state: FSMContext):
+    
+    await callback.answer()
 
     data = await state.get_data()
+    user_id = data["user_id"]
+    username = data.get("username", "")
+    first_name = data.get("first_name", "")
+    camefrom = data.get("camefrom", "")
+    language = data.get("user_choice", "")
+    fluency = data.get("fluency", "in_making")
     lang_code = data.get("lang_code", "en")
-    gratitude_msg = QUESTIONARY["gratitude"][lang_code]
     message_mgr = data.get("message_mgr")
+    
+    gratitude_msg = QUESTIONARY["gratitude"][lang_code]
+    
     orig_message = data.get('orig_message')
 
     await callback.answer(
@@ -96,6 +113,9 @@ async def go_to_main_menu(callback: CallbackQuery, state: FSMContext):
     await message_mgr.delete_previous_messages(
         chat_id=callback.message.chat.id
     )
+    
+    # Сохраняем нового пользователя в БД
+    await database.create_user(user_id, username, first_name, camefrom, language, fluency, lang_code)
 
     # После сохранения сразу показываем главное меню
     await show_main_menu(orig_message, state)
