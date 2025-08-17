4d75aaf999bbf40b11704a2f4ef3e8047133af05
jebb <gabouchard2002@gmail.com>
2025-08-18 00:32:41 +0300

    made regular keyboard module

---
+++ bots/keyboards/regular_keyboards.py
@@ -0,0 +1,26 @@
+import sys
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+
+from translations import FIND_PARTNER, QUESTIONARY # noqa
+
+def show_location_keyboard(lang_code):
+
+    share_button = KeyboardButton(
+        text=QUESTIONARY["share_location"][lang_code],
+        request_location=True,
+        is_persistent=True,
+    )
+    cancel_button = KeyboardButton(
+        text=FIND_PARTNER["cancel"][lang_code]
+    )
+    markup = ReplyKeyboardMarkup(
+        keyboard=[
+            [share_button],
+            [cancel_button]
+        ],
+        resize_keyboard=True,
+    )
+    return markup
