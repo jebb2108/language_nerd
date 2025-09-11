be6e1262c978a6a9639c127d2b6e9c302eb960c0
jebb <gabouchard2002@gmail.com>
2025-09-11 21:14:54 +0300

    fixed bug

--- app/main.py
+++ app/main.py
@@ -164,4 +164,4 @@ app = FastAPI(lifespan=startup)
 app.include_router(router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8080, reload=True)
