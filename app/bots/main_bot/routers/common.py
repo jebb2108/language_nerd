98ba8d028a2f879799016afac5b5847d10a7ce0a
jebb <gabouchard2002@gmail.com>
2025-09-11 20:47:36 +0300

    enabled common router

--- app/bots/main_bot/routers/common.py
+++ app/bots/main_bot/routers/common.py
@@ -4,11 +4,11 @@ from aiogram.types import Message
 router = Router(name=__name__)
 
 
-# @router.message()
-# async def echo(message: Message):
-#     if message.text:
-#         await message.copy_to(message.chat.id)
-#     else:
-#         await message.bot.send_message(
-#             chat_id=message.chat.id, text="press /help to get help"
-#         )
+@router.message()
+async def echo(message: Message):
+    if message.text:
+        await message.copy_to(message.chat.id)
+    else:
+        await message.bot.send_message(
+            chat_id=message.chat.id, text="press /help to get help"
+        )
