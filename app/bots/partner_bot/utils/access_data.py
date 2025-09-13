e56b839feb97159070c7a112713c681b9ff28e51
jebb <gabouchard2002@gmail.com>
2025-09-13 19:21:56 +0300

    trying to fix bug w/ get_storage func

--- app/bots/partner_bot/utils/access_data.py
+++ app/bots/partner_bot/utils/access_data.py
@@ -1,81 +1,68 @@
 import asyncio
 from datetime import datetime, time
-
 from aiogram.fsm.context import FSMContext
-
 from app.bots.partner_bot.middlewares.resources_middleware import ResourcesMiddleware
 from app.bots.partner_bot.utils.exc import StorageDataException
 
 
 class DataStorage:
     def __init__(self):
-        self.lock = None
+        self.lock = asyncio.Lock()
 
     async def get_storage_data(
         self, user_id: int, state: FSMContext, database: ResourcesMiddleware
     ) -> dict:
         """Достаем нужные данные о пользователе"""
-        if self.lock is None:
-            self.lock = asyncio.Lock()
-
         async with self.lock:
-
             s_data = await state.get_data()
 
             # Проверяем наличие необходимых ключей
             keys = ["user_id", "username", "first_name", "lang_code"]
-            data_status = all(key in s_data for key in keys)
+            if all(key in s_data for key in keys):
+                return s_data
 
-            if not data_status:
-                user_data = await self.set_user_info(user_id, database)
-                if user_data:
-                    await state.update_data(user_data)
-                    return await state.get_data()
-                else:
-                    raise StorageDataException("Error while trying to access user data from database")
+            # Если данных нет в Redis, получаем из базы и сохраняем в Redis
+            user_data = await self.set_user_info(user_id, database)
+            if not user_data:
+                raise StorageDataException(
+                    "Error while trying to access user data from database"
+                )
 
+            await state.update_data(user_data)
+            return user_data
 
     @staticmethod
     async def set_user_info(user_id: int, database: ResourcesMiddleware) -> dict:
         """Гарантирует, что машина состояние имеет все данные о пользователе"""
-        s_data = dict()
-        if user_info := await database.get_user_info(user_id):
-            if profile_info := await database.get_users_profile(user_id):
+        user_info = await database.get_user_info(user_id)
+        if not user_info:
+            return {}
 
-                birthday = profile_info["birthday"]
-                if isinstance(birthday, datetime):
-                    age_delta = datetime.now() - birthday
-                else:
-                    # Предполагаем, что birthday это date или строка
-                    birthday_date = datetime.combine(birthday, time.min)
-                    age_delta = datetime.now() - birthday_date
+        profile_info = await database.get_users_profile(user_id)
+        result = {
+            "user_id": user_id,
+            "username": user_info["username"],
+            "first_name": user_info["first_name"],
+            "language": user_info["language"],
+            "fluency": user_info["fluency"],
+            "lang_code": user_info["lang_code"],
+        }
 
-                s_data.update(
-                    user_id=user_id,
-                    username=user_info["username"],
-                    first_name=user_info["first_name"],
-                    language=user_info["language"],
-                    fluency=user_info["fluency"],
-                    lang_code=user_info["lang_code"],
-                    age=age_delta.days // 365,
-                    pref_name=profile_info["prefered_name"],
-                    status=profile_info["status"],
-                    about=profile_info["about"],
-                )
-                return s_data
+        if profile_info:
+            birthday = profile_info["birthday"]
+            if not isinstance(birthday, datetime):
+                birthday = datetime.combine(birthday, time.min)
 
-            s_data.update(
-                user_id=user_info["user_id"],
-                username=user_info["username"],
-                first_name=user_info["first_name"],
-                language=user_info["language"],
-                fluency=user_info["fluency"],
-                lang_code=user_info["lang_code"],
+            result.update(
+                {
+                    "age": (datetime.now() - birthday).days // 365,
+                    "pref_name": profile_info["prefered_name"],
+                    "status": profile_info["status"],
+                    "about": profile_info["about"],
+                }
             )
 
-            return s_data
-
-        return {}
+        return result
 
 
 data_storage = DataStorage()
