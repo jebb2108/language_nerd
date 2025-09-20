8a7d41993386ba31563d721187577e1ca5c1e96a
jebb <gabouchard2002@gmail.com>
2025-09-20 09:46:56 +0300

    fixed bugs

--- app/bots/main_bot/routers/__init__.py
+++ app/bots/main_bot/routers/__init__.py
@@ -4,7 +4,7 @@ from aiogram import Router
 
 from .commands import router as commands_router
 from .callback_handlers import router as callback_handlers_router
-from .admin_commands import router as admin_commands_router
+# from .admin_commands import router as admin_commands_router
 from .common import router as common_router
 
 router = Router(name=__name__)
@@ -12,7 +12,7 @@ router = Router(name=__name__)
 router.include_routers(
     commands_router,
     callback_handlers_router,
-    admin_commands_router,
+    # admin_commands_router,
 )
 
 # this has to be the last!
