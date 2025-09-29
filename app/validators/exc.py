e81a9147703e5213faa4a91274930b8220e9c976
jebb <gabouchard2002@gmail.com>
2025-09-29 15:43:20 +0300

    fixed bug

--- app/validators/exc.py
+++ app/validators/exc.py
@@ -1,14 +1,14 @@
-class EmptySpaceError(BaseException):
-    raise "Nickname cannot contain empty spaces"
+class EmptySpaceError(Exception):
+    pass
 
-class TooShortError(BaseException):
-    raise "Nickname must be at least 6 characters long"
+class TooShortError(Exception):
+    pass
 
-class TooLongError(BaseException):
-    raise "Nickname must be at most 16 characters long"
+class TooLongError(Exception):
+    pass
 
-class AlreadyExistsError(BaseException):
-    raise "Nickname already exists"
+class AlreadyExistsError(Exception):
+    pass
 
-class InvalidCharactersError(BaseException):
-    raise "Nickname must contain only latin characters and numbers"
+class InvalidCharactersError(Exception):
+    pass
