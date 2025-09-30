372456ad12bf745fb94364377284ead23a59b506
jebb <gabouchard2002@gmail.com>
2025-10-01 00:34:44 +0300

    fixed bug

--- app/bots/main_bot/utils/paytime.py
+++ app/bots/main_bot/utils/paytime.py
@@ -5,8 +5,7 @@ from app.dependencies import get_db
 
 async def paytime(user_id: int):
     db = await get_db()
-    created_at = await db.get_users_due_to(user_id)
-    created_at_obj = datetime.strptime(created_at, "%Y-%m-%d %H:%M:%S")
-    if created_at_obj + timedelta(days=3) > datetime.now() - timedelta(days=4):
+    due_to = await db.get_users_due_to(user_id)
+    if due_to + timedelta(days=3) > datetime.now() - timedelta(days=4):
         return True
     return False
