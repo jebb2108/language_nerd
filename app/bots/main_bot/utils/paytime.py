cc4e4909bb9e02a9fee6c7c5df0b26c05649a79d
jebb <gabouchard2002@gmail.com>
2025-10-03 00:45:36 +0300

    fixed bug

--- app/bots/main_bot/utils/paytime.py
+++ app/bots/main_bot/utils/paytime.py
@@ -6,11 +6,11 @@ async def paytime(user_id: int):
     db = await get_db()
     redis_client = await get_redis_client()
     due_to = await redis_client.get(f"user_paid:{user_id}")
-    if due_to and due_to > datetime.now():
+    if due_to and datetime.fromisoformat(due_to) > datetime.now():
             return True
     elif due_to := await db.get_users_due_to(user_id):
         if due_to > datetime.now():
-            await redis_client.setex(f"user_paid:{user_id}", 3600, due_to)
+            await redis_client.setex(f"user_paid:{user_id}", 3600, due_to.isoformat())
             return True
 
     return False
