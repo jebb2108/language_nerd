20e022e2bf20036578ad800c3f69fab365f1e116
jebb <gabouchard2002@gmail.com>
2025-10-03 00:42:08 +0300

    fixed bug

--- app/bots/main_bot/utils/paytime.py
+++ app/bots/main_bot/utils/paytime.py
@@ -1,16 +1,18 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from app.dependencies import get_db, get_redis_client
+from config import config
 
 
 async def paytime(user_id: int):
-    """Проверяет, не истекла ли подписка пользователя"""
+    """ Проверяет, не истекла ли подписка пользователя """
     db = await get_db()
     redis_client = await get_redis_client()
     due_to = await redis_client.get(f"user_paid:{user_id}")
-    if due_to and due_to + timedelta(days=3) > datetime.now():
-        return True
-    elif due_to := await db.get_users_due_to(user_id) and due_to + timedelta(days=3) > datetime.now():
-        await redis_client.setex(f"user_paid:{user_id}", 3600, due_to)
-        return True
+    if due_to and due_to > datetime.now():
+            return True
+    elif due_to := await db.get_users_due_to(user_id):
+        if due_to > datetime.now(tz=config.TZINFO):
+            await redis_client.setex(f"user_paid:{user_id}", 3600, due_to)
+            return True
 
     return False
