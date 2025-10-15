6fe76e81cbe6f49e7102d0d29e2f0f14688103df
jebb <gabouchard2002@gmail.com>
2025-10-15 17:14:36 +0300

    fixed DB issue

--- app/services/database.py
+++ app/services/database.py
@@ -160,7 +160,8 @@ class DatabaseService:
                 id SERIAL PRIMARY KEY,
                 user_id BIGINT NOT NULL,
                 payment_method_id TEXT NOT NULL,
-                updated_at TIMESTAMP DEFAULT NOW()
+                updated_at TIMESTAMP DEFAULT NOW(),
+                UNIQUE (user_id) 
                 );"""
             )
 
