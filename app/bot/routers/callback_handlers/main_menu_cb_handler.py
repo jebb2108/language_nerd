d026e47da4849bdbc1a2ad154d09f7534e8f7aca
jebb <gabouchard2002@gmail.com>
2025-10-18 12:19:50 +0300

    fixed bug

--- app/bot/routers/callback_handlers/main_menu_cb_handler.py
+++ app/bot/routers/callback_handlers/main_menu_cb_handler.py
@@ -149,8 +149,8 @@ async def profile_handler(callback: CallbackQuery, state: FSMContext):
 
 
         nickname = data.get("nickname", callback.from_user.username)
-        sidebar = "=" * (18 - len(nickname))
-        formated_nickname = sidebar + nickname + sidebar
+        sidebar = "=" * (15 - len(nickname))
+        formated_nickname = sidebar + " " + nickname + " " + sidebar
         topics = [TRANSCRIPTIONS["topics"][topic][lang_code] for topic in data.get("topics").split(", ")]
         msg = MESSAGES["user_info"][lang_code].format(
             nickname=formated_nickname,
